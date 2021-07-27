c2a4ac89efe63fb89cc894d1bf895a97abf9bbc8
Danilo Perez <daniloperez2016@outlook.com>
2021-07-26 21:28:42 -0300

    Cliente, TipoCliente, Telefones e Endereços

---
+++ src/main/java/com/daniloperez/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.daniloperez.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniloperez.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+}
